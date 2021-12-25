b02c90e644fcc2e26b8853e233178701183f609d
Hezbon12 <otienohezbon12@gmail.com>
2021-12-26 00:18:43 +0300

    Create __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+import os
 from models.base_model import BaseModel
+from models.base_model import Base
 from models.user import User
 from models.amenity import Amenity
 from models.city import City
@@ -7,7 +9,15 @@ from models.place import Place
 from models.state import State
 from models.review import Review
 from models.engine import file_storage
+from models.engine import db_storage
 
 
-storage = file_storage.FileStorage()
+try:
+    if os.environ['HBNB_TYPE_STORAGE'] == "db":
+        storage = db_storage.DBStorage()
+    else:
+        raise KeyError
+except KeyError:
+    storage = file_storage.FileStorage()
+
 storage.reload()
