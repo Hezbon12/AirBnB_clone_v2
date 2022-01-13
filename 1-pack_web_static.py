34b77fc232f7a66558af25b0c5d47013dbda207b
Hezbon12 <otienohezbon12@gmail.com>
2022-01-13 22:39:31 +0300

    deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""Compress web static package
 """
-from fabric.api import local
-from datetime import datetime
+file to practice use of Fabric
+"""
+from fabric.api import *
+import time
 
 
 def do_pack():
-    """Function to compress directory
-    Return: path to archive on success; None on fail
-    """
-    # Get current time
-    now = datetime.now()
-    now = now.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + now + '.tgz'
-
-    # Create archive
-    local('mkdir -p versions/')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
-
-    # Check if archiving was successful
-    if result.succeeded:
-        return archive_path
-    return None
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+                format(timestr))
+        return ("versions/web_static_{}.tgz".format(timestr))
+    except:
+        return None
